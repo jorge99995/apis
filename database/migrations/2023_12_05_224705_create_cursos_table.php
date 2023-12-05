61801c34a22ec6b98e5b2dfba3359377eead71de
unknown <jorge02vilanueva@gmail.com>
2023-12-05 18:10:09 -0500

    first commit

---
+++ database/migrations/2023_12_05_224705_create_cursos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCursosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->bigIncrements('id_curso');
+            $table->string('nombre_curso');
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('id_cliente');
+
+            $table->foreign('categoria_id')->references('id_categoria')->on('categorias');
+            $table->foreign('id_cliente')->references('cliente_id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cursos');
+    }
+}
